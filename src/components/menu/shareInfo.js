c69fbb0bdff2ca1d6fe748a548ed4c44cd7326e9
Charlie Jung <32102793+charliecjung@users.noreply.github.com>
2019-07-04 13:38:12 +0900

    added a sample telegram share icon

--- src/components/menu/shareInfo.js
+++ src/components/menu/shareInfo.js
@@ -1,6 +1,13 @@
 import React from 'react'
 import { addBtnTouchListener } from '../../util'
 
+import {TelegramShareButton} from 'react-share'
+
+import {TelegramIcon} from 'react-share'
+
+
+
+
 import Storage from '../../storage.js'
 
 class ShareInfo extends React.Component {
@@ -17,10 +24,25 @@ class ShareInfo extends React.Component {
 
   render () {
     let data = this.props
+    const shareUrl = "https://www.github.com"
+    const title = "Github"
     return (
-    <div>
-     hi   
+
+    <div className="share-component">
+      <h1> Share with the World! </h1>
+      <TelegramShareButton
+        url={shareUrl}
+        title={title}
+        className="telegram-share-button">
+        <TelegramIcon
+        size={32}
+        round />
+        </TelegramShareButton>
+        
+      
+    
     </div>
+   
     )
     
   
